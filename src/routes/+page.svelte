98d2b91fc1fa848abb3d1041b04ad7f62a005325
AndreyHP <andreymarins501@gmail.com>
2025-01-15 16:43:09 -0300

    start of the site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,4 @@
-<style>
-	.main-page {
-		background-color: goldenrod;
-		font-family: 'Comic Sans MD', cursive;
-	}
-</style>
-
-
-
-<h1>Sakura Reader</h1>
+<h1 class="page-title">Sakura Reader</h1>
 <body class="main-page">
- sdad
+ <p>日本語を読みます</p>
 </body>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<body class="main-page">
+ sdad
+</body>
