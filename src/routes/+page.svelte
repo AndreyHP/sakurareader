05110b750f03c06b7ce7baeeef18860773f67336
AndreyHP <andreymarins501@gmail.com>
2025-01-15 14:54:25 -0300

    first-commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<style>
+	.main-page {
+		background-color: goldenrod;
+		font-family: 'Comic Sans MD', cursive;
+	}
+</style>
+
+
+
+<h1>Sakura Reader</h1>
+<body class="main-page">
+ sdad
+</body>
