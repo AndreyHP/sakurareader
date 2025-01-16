fe392f1bf459327952af945ab843f48c94afb9d3
AndreyHP <andreymarins501@gmail.com>
2025-01-16 09:49:53 -0300

    build-update

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ export default {
 			strict: true
 		}),
 		paths: {
-      base: process.env.NODE_ENV === 'production' ? '/AndreyHP/sakurareader' : '',
+      base: process.env.NODE_ENV === 'production' ? '/sakurareader' : '',
     },
 	}
 };
