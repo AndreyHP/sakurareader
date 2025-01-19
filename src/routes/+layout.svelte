27749489bdb642ca2643bf75a581c395d4a2520b
AndreyHP <andreymarins501@gmail.com>
2025-01-19 10:08:53 -0300

    change the link label

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,10 @@
 <header>
 	<div id="menu-bar">
     <nav>
-	    <a href="{base}/" class="menu-link" title="home">
+	    <a href="{base}/" class="menu-link" title="home" aria-label="Go back to Home page">
                 <img src="./logo.png" id="img-logo" alt="sakura">
         </a>
-	    <a href="{base}/about" class="menu-link" title="settings">
+	    <a href="{base}/about" class="menu-link" title="settings" aria-label="Go to settings">
                 <img src="./gear-solid.png" id="settings-logo" alt="settings">
         </a>
     </nav>
